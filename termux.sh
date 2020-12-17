a241f8822c540ca536bc3792c824f658b8220914
dizun4399 <2841113695@qq.com>
2020-12-17 20:17:39 +0800

    first configration

--- termux.sh
+++ termux.sh
@@ -3,7 +3,7 @@ sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/t
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
 apt update && apt upgrade
-pkg install proot -y
+pkg install proot
 pkg install mariadb
 pkg install nginx
 pkg install php-fpm
